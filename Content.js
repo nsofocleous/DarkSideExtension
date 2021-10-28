d399c8f574b9a2290d8e50c6d5d746b216c09dfd
Robert <robert.koulakes@gmail.com>
2021-10-27 19:43:42 -0700

    Adding Chrome extension files

---
+++ Content.js
@@ -0,0 +1,6 @@
+//document.getElementsByTagName("body")[0].style = "background-color: black";
+//document.getElementsByTagName("p")[0].style = "color: white";
+
+//If button is on, darken background, and play sound sample
+
+//If button is off, reverse changes, and play sound sample
